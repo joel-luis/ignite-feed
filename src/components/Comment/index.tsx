e7753bcb79e65a54f62516a2e6b00fe43898f572
Joel Luis <joelluis73@gmail.com>
2022-10-22 13:34:26 -0300

    create Comment component

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,32 @@
+import { Trash, ThumbsUp } from 'phosphor-react'
+import styles from './styles.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/joel-luis.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Joel Luís</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
